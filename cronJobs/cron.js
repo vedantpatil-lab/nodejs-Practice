ed172e20de4f0d8231596f49d3a47c93336ba230
Vedant Patil <vedant.patil@nimapinfotech.com>
2026-01-31 10:43:10 +0530

    Crons

--- cronJobs/cron.js
+++ cronJobs/cron.js
@@ -1,16 +1,52 @@
-const cron = require("node-cron")
-const { sendMail } = require("./utils/sendEmail")
+// const cron = require("node-cron")
+// const { sendMail } = require("./utils/sendEmail")
+
+// // cron.schedule("* * * * *", ()=>{
+// //     console.log("Cron job is running !!")
+// // })
 
 // cron.schedule("* * * * *", ()=>{
-//     console.log("Cron job is running !!")
+//     const to = "vedant.patil@nimapinfotech.com"
+//     const subject = "Cron Job Check"
+//     const body = "The mail is for checking cron jobs"
+
+//     sendMail(to, subject, body)
+//     console.log("mail sent")
 // })
 
-cron.schedule("* * * * *", ()=>{
-    const to = "vedant.patil@nimapinfotech.com"
-    const subject = "Cron Job Check"
-    const body = "The mail is for checking cron jobs"
+// cron.schedule(
+//   "0 9 * * *",
+//   () => {
+//     sendReport();
+//   },
+//   {
+//     timezone: "Asia/Kolkata"
+//   }
+// );
 
-    sendMail(to, subject, body)
-    console.log("mail sent")
-})
+// cron.schedule("* * * * *", async () => {
+//   try {
+//     await sendEmail();
+//   } catch (err) {
+//     console.error("Cron failed:", err);
+//   }
+// });
+
+// async function retry(fn, retries = 3) {
+//   while (retries--) {
+//     try {
+//       return await fn();
+//     } catch (err) {
+//       if (retries === 0) throw err;
+//     }
+//   }
+// }
+
+// cron.schedule("*/5 * * * *", async () => {
+//   await retry(() => sendEmail());
+// });
+
+// cron.schedule("0 10 * * *", async () => {
+//   await subscriptionService.sendExpiryReminders();
+// });
 
